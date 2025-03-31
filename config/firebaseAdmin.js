f958b62375bf6c5c721fbccdd5d7c6fc547edb3e
Ansh Sharma <137365987+anxious2004@users.noreply.github.com>
2025-03-31 15:49:04 +0530

    Update firebaseAdmin.js

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -2,12 +2,7 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./firebase-adminsdk.json"); // Ensure this file exists
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), 
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    })
+    credential: admin.credential.cert(serviceAccount),
 });
-
 
 module.exports = admin;
